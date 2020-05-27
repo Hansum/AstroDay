020ec8a88115b14738b0086a5cd2a96ec8e03f80
hansum <christianvargan@gmail.com>
2020-05-27 20:13:10 +0800

    used tailblock for designing

--- astroday/pages/index.js
+++ astroday/pages/index.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import PictureinfoComponent from "../components/PictureinfoComponent";
 import useSWR, { mutate } from "swr";
 import HomeComponent from "../components/HomeComponent";
+import search from "../public/searching.svg";
 
 let getDateToday = () => {
   return new Date().toISOString().slice(0, 10);
@@ -46,9 +47,18 @@ const PictureOftheDay = () => {
   );
 
   if (error) console.log("error loading data");
-  if (!data) return <div>Fetching Picture</div>;
+  if (!data) {
+    return (
+      <div className="flex h-screen">
+        <div className="m-auto">
+          <img className="mh-64 w-64" src={search} alt="searching photo"></img>
+          <h1 className="text-center">Fetching Picture</h1>
+        </div>
+      </div>
+    );
+  }
   console.log("Data:", DateToday);
-
+  console.log("photographer", data.copyright);
   return (
     <HomeComponent>
       <div className="flex justify-center">
@@ -66,20 +76,96 @@ const PictureOftheDay = () => {
           />
         </div>
       </div>
-      <h1 className="text-3xl text-center mt-20">PICTURE OF THE DAY</h1>
+      {DateToday === data.date ? (
+        <h1 className="text-3xl text-center mt-20">PICTURE OF THE DAY</h1>
+      ) : (
+        <h1 className="text-3xl text-center mt-20">Picture of {data.date}</h1>
+      )}
       {data.url ? (
         <div>
-          <img
-            src={data.url}
-            alt="picture of the day"
-            className="block m-auto"
-          ></img>
-          <PictureinfoComponent
-            title={data.title}
-            date={data.date}
-            photographer={data.copyright}
-            description={data.explanation}
-          ></PictureinfoComponent>
+          <section className="text-gray-700 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+              <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+                <img
+                  alt="feature"
+                  className="object-cover object-center h-full w-full"
+                  src={data.url}
+                />
+              </div>
+              <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                <div className="flex flex-col mb-10 lg:items-start items-center">
+                  <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-teal-100 text-teal-500 mb-5">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle cx="6" cy="6" r="3"></circle>
+                      <circle cx="6" cy="18" r="3"></circle>
+                      <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 text-lg title-font font-bold mb-3">
+                      Title
+                    </h2>
+                    <p className="leading-relaxed text-base">{data.title}</p>
+                  </div>
+                </div>
+                <div className="flex flex-col mb-10 lg:items-start items-center">
+                  <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-teal-100 text-teal-500 mb-5">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 text-lg title-font font-bold mb-3">
+                      Photographer
+                    </h2>
+                    <p className="leading-relaxed text-base">
+                      {data.copyright ? data.copyright : "N / A"}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col mb-10 lg:items-start items-center">
+                  <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-teal-100 text-teal-500 mb-5">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-6 h-6"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 text-lg title-font font-bold mb-3">
+                      Description
+                    </h2>
+                    <p className="leading-relaxed text-base">
+                      {data.explanation}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       ) : (
         "no image found"
--- astroday/tailwind.config.js
+++ astroday/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     fontFamily: {
       poppins: ["Poppins"],
     },
+    inset: {
+      "1/2": "50%",
+    },
+    top: {
+      "1/2": "50%",
+    },
+    left: {
+      "1/2": "50%",
+    },
   },
   variants: {},
   plugins: [],
